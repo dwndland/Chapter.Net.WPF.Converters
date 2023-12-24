8dcc20bab81485bd983d255237e24d83096f6fb7
David Wendland <dwndland@gmail.com>
2023-12-24 21:25:42 +0100

    Add demo project

---
+++ Demo/IntegerItem.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="IntegerItem.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+namespace Demo;
+
+public class IntegerItem
+{
+    public IntegerItem(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; }
+}
---
+++ Demo/MainView.xaml.cs
@@ -0,0 +1,16 @@
+﻿// 
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// 
+
+namespace Demo;
+
+public partial class MainView
+{
+    public MainView()
+    {
+        InitializeComponent();
+
+        DataContext = new MainViewModel();
+    }
+}
