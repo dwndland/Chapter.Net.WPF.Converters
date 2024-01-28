1203be3c1355f2d7da4dcb284b3b95023bf14517
David Wendland <dwndland@gmail.com>
2024-01-28 21:41:12 +0100

    Add missing multi convert tests for the EqualsToBooleanConverterTests

--- Chapter.Net.WPF.Converters.Tests/EqualsToBooleanConverter/EqualsToBooleanConverterTests.cs
+++ Chapter.Net.WPF.Converters.Tests/EqualsToBooleanConverter/EqualsToBooleanConverterTests.cs
@@ -11,7 +11,7 @@ using NUnit.Framework;
 
 namespace Chapter.Net.WPF.Converters.Tests;
 
-public class EqualsToBooleanConverterTests : ValueConverterTester<EqualsToBooleanConverter>
+public class EqualsToBooleanConverterTests : SingleAndMultiValueConverterTester<EqualsToBooleanConverter>
 {
     [TestCase(1, 1, true, false, true)]
     [TestCase(1, 1, false, true, false)]
@@ -37,9 +37,38 @@ public class EqualsToBooleanConverterTests : ValueConverterTester<EqualsToBoolea
         Convert(first, expectation);
     }
 
+    [TestCase(1, 1, true, false, true)]
+    [TestCase(1, 1, false, true, false)]
+    [TestCase(1, 1, null, false, null)]
+    [TestCase(1, 1.0, true, false, false)]
+    [TestCase(1, 1.0, false, true, true)]
+    [TestCase(1, 1.0, true, null, null)]
+    [TestCase(1, 2, true, false, false)]
+    [TestCase(1, 2, false, true, true)]
+    [TestCase(1, 2, true, null, null)]
+    [TestCase("First", "First", true, false, true)]
+    [TestCase("First", "First", false, true, false)]
+    [TestCase("First", "First", null, false, null)]
+    [TestCase("First", "Second", true, false, false)]
+    [TestCase("First", "Second", false, true, true)]
+    [TestCase("First", "Second", true, null, null)]
+    public void MultiConvert_Called_Compares(object first, object second, bool? isEqual, bool? isNotEqual, bool? expectation)
+    {
+        _target.IsEqual = isEqual;
+        _target.IsNotEqual = isNotEqual;
+
+        MultiConvert([first, second], expectation);
+    }
+
     [Test]
     public void ConvertBack_Called_RaisesException()
     {
         Assert.That(() => ConvertBack(null, null), Throws.TypeOf<NotImplementedException>());
+    }
+
+    [Test]
+    public void MultiConvertBack_Called_RaisesException()
+    {
+        Assert.That(() => MultiConvertBack(null, Array.Empty<object>()), Throws.TypeOf<NotImplementedException>());
     }
 }
