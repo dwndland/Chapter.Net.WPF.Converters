305d3445948654618afb12d44474bb94fac25292
David Wendland <dwndland@gmail.com>
2024-01-28 21:51:55 +0100

    Added IntegerComparisonToBooleanConverter to execute a comparison on a single integer or list of integers to a boolean representation.

---
+++ Chapter.Net.WPF.Converters.Tests/IntegerComparisonToBooleanConverter/IntegerComparisonToBooleanConverterTests.cs
@@ -0,0 +1,86 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="IntegerComparisonToBooleanConverterTests.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+using System;
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+
+namespace Chapter.Net.WPF.Converters.Tests;
+
+public class IntegerComparisonToBooleanConverterTests : SingleAndMultiValueConverterTester<IntegerComparisonToBooleanConverter>
+{
+    [TestCase(NumberComparisonType.BiggerThan, true, false, 5, 6, true)]
+    [TestCase(NumberComparisonType.BiggerThan, false, false, 5, 6, false)]
+    [TestCase(NumberComparisonType.BiggerThan, null, false, 5, 6, null)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, 5, 4, false)]
+    [TestCase(NumberComparisonType.BiggerThan, true, true, 5, 4, true)]
+    [TestCase(NumberComparisonType.BiggerThan, true, null, 5, 4, null)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, 5, "", false)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, 5, null, false)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, 5, 4, true)]
+    [TestCase(NumberComparisonType.SmallerThan, false, false, 5, 4, false)]
+    [TestCase(NumberComparisonType.SmallerThan, null, false, 5, 4, null)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, 5, 6, false)]
+    [TestCase(NumberComparisonType.SmallerThan, true, true, 5, 6, true)]
+    [TestCase(NumberComparisonType.SmallerThan, true, null, 5, 6, null)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, 5, "", false)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, 5, null, false)]
+    public void Convert_Called_Converts(NumberComparisonType comparisonType, bool? trueIs, bool? falseIs, int variable, object input, bool? expectation)
+    {
+        _target.ComparisonType = comparisonType;
+        _target.TrueIs = trueIs;
+        _target.FalseIs = falseIs;
+        _target.Variable = variable;
+
+        Convert(input, expectation);
+    }
+
+    [TestCase(NumberComparisonType.BiggerThan, true, false, null, 5, true, 6, 7, 8)]
+    [TestCase(NumberComparisonType.BiggerThan, false, false, null, 5, false, 6, 7, 8)]
+    [TestCase(NumberComparisonType.BiggerThan, null, false, null, 5, null, 6, 7, 8)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, null, 5, false, 4, 4, 4)]
+    [TestCase(NumberComparisonType.BiggerThan, true, true, null, 5, true, 4, 4, 4)]
+    [TestCase(NumberComparisonType.BiggerThan, true, null, null, 5, null, 4, 4, 4)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, null, 5, null, 4, 6, 4)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, true, 5, true, 4, 6, 4)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, false, 5, false, 4, 6, 4)]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, null, 5, false, "")]
+    [TestCase(NumberComparisonType.BiggerThan, true, false, null, 5, false, null)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, null, 5, true, 4, 3, 2)]
+    [TestCase(NumberComparisonType.SmallerThan, false, false, null, 5, false, 4, 3, 2)]
+    [TestCase(NumberComparisonType.SmallerThan, null, false, null, 5, null, 4, 3, 2)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, null, 5, false, 6, 7, 8)]
+    [TestCase(NumberComparisonType.SmallerThan, true, true, null, 5, true, 6, 7, 8)]
+    [TestCase(NumberComparisonType.SmallerThan, true, null, null, 5, null, 6, 7, 8)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, null, 5, null, 6, 4, 8)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, true, 5, true, 6, 4, 8)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, false, 5, false, 6, 4, 8)]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, null, 5, false, "")]
+    [TestCase(NumberComparisonType.SmallerThan, true, false, null, 5, false, null)]
+    public void Convert_Called_Converts(NumberComparisonType comparisonType, bool? trueIs, bool? falseIs, bool? mixedIs, int variable, bool? expectation, params object[] input)
+    {
+        _target.ComparisonType = comparisonType;
+        _target.TrueIs = trueIs;
+        _target.FalseIs = falseIs;
+        _target.MixedIs = mixedIs;
+        _target.Variable = variable;
+
+        MultiConvert(input, expectation);
+    }
+
+    [Test]
+    public void ConvertBack_Called_RaisesException()
+    {
+        Assert.That(() => ConvertBack(null, ""), Throws.TypeOf<NotImplementedException>());
+    }
+
+    [Test]
+    public void MultiConvertBack_Called_RaisesException()
+    {
+        Assert.That(() => MultiConvertBack(null, Array.Empty<object>()), Throws.TypeOf<NotImplementedException>());
+    }
+}
---
+++ Chapter.Net.WPF.Converters/IntegerComparisonToBooleanConverter/IntegerComparisonToBooleanConverter.cs
@@ -0,0 +1,106 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="IntegerComparisonToBooleanConverter.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+#pragma warning disable CA2208
+// ReSharper disable once CheckNamespace
+
+namespace Chapter.Net.WPF.Converters;
+
+/// <summary>
+///     Executes a comparison on a single integer or list of integers to a boolean representation.
+/// </summary>
+[ValueConversion(typeof(int), typeof(bool))]
+[ValueConversion(typeof(int[]), typeof(bool))]
+public class IntegerComparisonToBooleanConverter : SingleAndMultiValueConverter
+{
+    /// <summary>
+    ///     Defines the comparison type.
+    /// </summary>
+    /// <value>Default: NumberComparisonType.BiggerThan.</value>
+    [DefaultValue(NumberComparisonType.BiggerThan)]
+    public NumberComparisonType ComparisonType { get; set; } = NumberComparisonType.BiggerThan;
+
+    /// <summary>
+    ///     The variable to compare with.
+    /// </summary>
+    /// <value>Default: 0.</value>
+    [DefaultValue(0)]
+    public int Variable { get; set; } = 0;
+
+    /// <summary>
+    ///     The return if the comparison is true.
+    /// </summary>
+    /// <value>Default: true.</value>
+    [DefaultValue(true)]
+    public bool? TrueIs { get; set; } = true;
+
+    /// <summary>
+    ///     The return if the comparison is false.
+    /// </summary>
+    /// <value>Default: false.</value>
+    [DefaultValue(false)]
+    public bool? FalseIs { get; set; } = false;
+
+    /// <summary>
+    ///     The return if the comparisons are mixed.
+    /// </summary>
+    /// <value>Default: false.</value>
+    [DefaultValue(false)]
+    public bool? MixedIs { get; set; } = null;
+
+    /// <summary>
+    ///     Executes a comparison on a single integer to a boolean representation.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <param name="targetType">Unused.</param>
+    /// <param name="parameter">Unused.</param>
+    /// <param name="culture">Unused.</param>
+    /// <returns>The converted value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">ComparisonType got extended but not covered.</exception>
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is int integer ? Compare(integer) : FalseIs;
+    }
+
+    /// <summary>
+    ///     Executes a comparison on a list of integers to a boolean representation.
+    /// </summary>
+    /// <param name="values">The values to convert.</param>
+    /// <param name="targetType">Unused.</param>
+    /// <param name="parameter">Unused.</param>
+    /// <param name="culture">Unused.</param>
+    /// <returns>The converted value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">ComparisonType got extended but not covered.</exception>
+    public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null)
+            return FalseIs;
+
+        var numbers = values.OfType<int>().Select(Compare).Distinct().ToList();
+        return numbers.Count switch
+        {
+            0 => FalseIs,
+            > 1 => MixedIs,
+            _ => numbers[0]
+        };
+    }
+
+    private bool? Compare(int number)
+    {
+        return ComparisonType switch
+        {
+            NumberComparisonType.BiggerThan => number > Variable ? TrueIs : FalseIs,
+            NumberComparisonType.SmallerThan => number < Variable ? TrueIs : FalseIs,
+            _ => throw new ArgumentOutOfRangeException(nameof(ComparisonType), ComparisonType, "ComparisonType got extended but not covered.")
+        };
+    }
+}
