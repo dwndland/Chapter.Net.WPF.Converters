d9d4bf7418268404ef226f27dcb53da9b84d8fed
David Wendland <dwndland@gmail.com>
2024-01-28 22:01:14 +0100

    Added IntegerMathConverter to do a simple calculation with the given value as integer.

---
+++ Chapter.Net.WPF.Converters.Tests/IntegerMathConverter/IntegerMathConverterTests.cs
@@ -0,0 +1,96 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="DoubleMathConverterTests.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+
+namespace Chapter.Net.WPF.Converters.Tests;
+
+public class IntegerMathConverterTests : ValueConverterTester<IntegerMathConverter>
+{
+    [TestCase(false, Calculation.Addition, 2, 2, 4)]
+    [TestCase(false, Calculation.Addition, 2, 2, 4)]
+    [TestCase(false, Calculation.Addition, (ushort)2, 2, 4)]
+    [TestCase(false, Calculation.Addition, null, 2, 0)]
+    [TestCase(false, Calculation.Subtraction, 4, 2, 2)]
+    [TestCase(false, Calculation.Subtraction, 4, 2, 2)]
+    [TestCase(false, Calculation.Subtraction, (ushort)4, 2, 2)]
+    [TestCase(false, Calculation.Subtraction, null, 2, 0)]
+    [TestCase(false, Calculation.Multiplication, 3, 3, 9)]
+    [TestCase(false, Calculation.Multiplication, 3, 3, 9)]
+    [TestCase(false, Calculation.Multiplication, (ushort)3, 3, 9)]
+    [TestCase(false, Calculation.Multiplication, null, 3, 0)]
+    [TestCase(false, Calculation.Division, 15, 3, 5)]
+    [TestCase(false, Calculation.Division, 15, 3, 5)]
+    [TestCase(false, Calculation.Division, (ushort)15, 3, 5)]
+    [TestCase(false, Calculation.Division, null, 3, 0)]
+    [TestCase(true, Calculation.Addition, 2, 2, 4)]
+    [TestCase(true, Calculation.Addition, 2, 2, 4)]
+    [TestCase(true, Calculation.Addition, (ushort)2, 2, 4)]
+    [TestCase(true, Calculation.Addition, null, 2, 0)]
+    [TestCase(true, Calculation.Subtraction, 4, 2, -2)]
+    [TestCase(true, Calculation.Subtraction, 4, 2, -2)]
+    [TestCase(true, Calculation.Subtraction, (ushort)4, 2, -2)]
+    [TestCase(true, Calculation.Subtraction, null, 2, 0)]
+    [TestCase(true, Calculation.Multiplication, 3, 3, 9)]
+    [TestCase(true, Calculation.Multiplication, 3, 3, 9)]
+    [TestCase(true, Calculation.Multiplication, (ushort)3, 3, 9)]
+    [TestCase(true, Calculation.Multiplication, null, 3, 0)]
+    [TestCase(true, Calculation.Division, 3, 15, 5)]
+    [TestCase(true, Calculation.Division, 3, 15, 5)]
+    [TestCase(true, Calculation.Division, (ushort)3, 15, 5)]
+    [TestCase(true, Calculation.Division, null, 3, 0)]
+    public void Convert_Called_Calculates(bool backwards, Calculation calculation, object input, int variable, int expectation)
+    {
+        _target.Backwards = backwards;
+        _target.Calculation = calculation;
+        _target.Variable = variable;
+
+        Convert(input, expectation);
+    }
+
+    [TestCase(false, Calculation.Addition, 4, 2, 2)]
+    [TestCase(false, Calculation.Addition, 4, 2, 2)]
+    [TestCase(false, Calculation.Addition, (ushort)2, 2, 0)]
+    [TestCase(false, Calculation.Addition, null, 2, 0)]
+    [TestCase(false, Calculation.Subtraction, 2, 2, 4)]
+    [TestCase(false, Calculation.Subtraction, 2, 2, 4)]
+    [TestCase(false, Calculation.Subtraction, (ushort)2, 2, 4)]
+    [TestCase(false, Calculation.Subtraction, null, 2, 0)]
+    [TestCase(false, Calculation.Multiplication, 9, 3, 3)]
+    [TestCase(false, Calculation.Multiplication, 9, 3, 3)]
+    [TestCase(false, Calculation.Multiplication, (ushort)9, 3, 3)]
+    [TestCase(false, Calculation.Multiplication, null, 3, 0)]
+    [TestCase(false, Calculation.Division, 5, 3, 15)]
+    [TestCase(false, Calculation.Division, 5, 3, 15)]
+    [TestCase(false, Calculation.Division, (ushort)5, 3, 15)]
+    [TestCase(false, Calculation.Division, null, 3, 0)]
+    [TestCase(true, Calculation.Addition, 4, 2, -2)]
+    [TestCase(true, Calculation.Addition, 4, 2, -2)]
+    [TestCase(true, Calculation.Addition, (ushort)2, 2, 0)]
+    [TestCase(true, Calculation.Addition, null, 2, 0)]
+    [TestCase(true, Calculation.Subtraction, 2, 2, 4)]
+    [TestCase(true, Calculation.Subtraction, 2, 2, 4)]
+    [TestCase(true, Calculation.Subtraction, (ushort)2, 2, 4)]
+    [TestCase(true, Calculation.Subtraction, null, 2, 0)]
+    [TestCase(true, Calculation.Multiplication, 3, 9, 3)]
+    [TestCase(true, Calculation.Multiplication, 3, 9, 3)]
+    [TestCase(true, Calculation.Multiplication, (ushort)3, 9, 3)]
+    [TestCase(true, Calculation.Multiplication, null, 3, 0)]
+    [TestCase(true, Calculation.Division, 5, 3, 15)]
+    [TestCase(true, Calculation.Division, 5, 3, 15)]
+    [TestCase(true, Calculation.Division, (ushort)5, 3, 15)]
+    [TestCase(true, Calculation.Division, null, 3, 0)]
+    public void ConvertBack_Called_Calculates(bool backwards, Calculation calculation, object input, int variable, int expectation)
+    {
+        _target.Backwards = backwards;
+        _target.Calculation = calculation;
+        _target.Variable = variable;
+
+        ConvertBack(input, expectation);
+    }
+}
---
+++ Chapter.Net.WPF.Converters/IntegerMathConverter/IntegerMathConverter.cs
@@ -0,0 +1,115 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="IntegerMathConverter.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+#pragma warning disable CA2208
+// ReSharper disable once CheckNamespace
+
+namespace Chapter.Net.WPF.Converters;
+
+/// <summary>
+///     Does a simple calculation with the given value as integer.
+/// </summary>
+[ValueConversion(typeof(int), typeof(int))]
+public class IntegerMathConverter : ValueConverter
+{
+    /// <summary>
+    ///     Gets or sets what operation shall be executed.
+    /// </summary>
+    /// <value>Default: Calculation.Addition.</value>
+    [DefaultValue(Calculation.Addition)]
+    public Calculation Calculation { get; set; } = Calculation.Addition;
+
+    /// <summary>
+    ///     Gets or sets the variable to calculate with.
+    /// </summary>
+    /// <value>Default: 1.</value>
+    [DefaultValue(1)]
+    public int Variable { get; set; } = 1;
+
+    /// <summary>
+    ///     Defines the direction of the calculation. False means (Value * Variable). True means (Variable * Value);
+    /// </summary>
+    /// <value>Default: false.</value>
+    [DefaultValue(false)]
+    public bool Backwards { get; set; } = false;
+
+    /// <summary>
+    ///     Does a simple calculation with the given value as integer.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <param name="targetType">Unused.</param>
+    /// <param name="parameter">Unused.</param>
+    /// <param name="culture">Unused.</param>
+    /// <returns>The converted value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Calculation got extended but not covered.</exception>
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value == null ? 0 : Calculate(System.Convert.ToInt32(value));
+    }
+
+    /// <summary>
+    ///     Does the backward simple calculation with the given value as integer.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <param name="targetType">Unused.</param>
+    /// <param name="parameter">Unused.</param>
+    /// <param name="culture">Unused.</param>
+    /// <returns>The converted value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Calculation got extended but not covered.</exception>
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value == null ? 0 : CalculateOpposite(System.Convert.ToInt32(value));
+    }
+
+    private int Calculate(int input)
+    {
+        if (Backwards)
+            return Calculation switch
+            {
+                Calculation.Addition => Variable + input,
+                Calculation.Subtraction => Variable - input,
+                Calculation.Multiplication => Variable * input,
+                Calculation.Division => Variable / input,
+                _ => throw new ArgumentOutOfRangeException(nameof(Calculation), Calculation, "Calculation got extended but not covered.")
+            };
+
+        return Calculation switch
+        {
+            Calculation.Addition => input + Variable,
+            Calculation.Subtraction => input - Variable,
+            Calculation.Multiplication => input * Variable,
+            Calculation.Division => input / Variable,
+            _ => throw new ArgumentOutOfRangeException(nameof(Calculation), Calculation, "Calculation got extended but not covered.")
+        };
+    }
+
+    private int CalculateOpposite(int input)
+    {
+        if (Backwards)
+            return Calculation switch
+            {
+                Calculation.Addition => Variable - input,
+                Calculation.Subtraction => Variable + input,
+                Calculation.Multiplication => Variable / input,
+                Calculation.Division => Variable * input,
+                _ => throw new ArgumentOutOfRangeException(nameof(Calculation), Calculation, "Calculation got extended but not covered.")
+            };
+
+        return Calculation switch
+        {
+            Calculation.Addition => input - Variable,
+            Calculation.Subtraction => input + Variable,
+            Calculation.Multiplication => input / Variable,
+            Calculation.Division => input * Variable,
+            _ => throw new ArgumentOutOfRangeException(nameof(Calculation), Calculation, "Calculation got extended but not covered.")
+        };
+    }
+}
