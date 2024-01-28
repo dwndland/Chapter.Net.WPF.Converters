d13d18fc826ff99ab630c58503fc6d4dad9216ba
David Wendland <dwndland@gmail.com>
2024-01-28 22:12:35 +0100

    Added IntegerToStringConverter to format a integer to string.

---
+++ Chapter.Net.WPF.Converters.Tests/IntegerToStringConverter/IntegerToStringConverterTests.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="IntegerToStringConverterTests.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+using System;
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+
+namespace Chapter.Net.WPF.Converters.Tests;
+
+public class IntegerToStringConverterTests : ValueConverterTester<IntegerToStringConverter>
+{
+    [TestCase(0, 2, "00")]
+    [TestCase(1, 3, "001")]
+    [TestCase(1.0, 1, "")]
+    [TestCase("Hans", 1, "")]
+    [TestCase(null, 1, "")]
+    public void Convert_Called_ReturnsExpected(object input, int digits, string expectation)
+    {
+        _target.Digits = digits;
+
+        Convert(input, expectation);
+    }
+
+    [TestCase("00", 0)]
+    [TestCase("001", 1)]
+    [TestCase(1.0, 0)]
+    [TestCase("Hans", 0)]
+    [TestCase(null, 0)]
+    public void ConvertBack_Called_Converts(object input, int expectation)
+    {
+        ConvertBack(input, expectation);
+    }
+}
