2c14a71ca8c32c73683e972c849b4699f247698d
David Wendland <dwndland@gmail.com>
2024-01-29 18:43:03 +0100

    Forgot to check in the integer or double math converter enum

---
+++ Chapter.Net.WPF.Converters/DoubleMathConverter/Calculation.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="Calculation.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+
+namespace Chapter.Net.WPF.Converters;
+
+/// <summary>
+///     Defines what calculation shall be done within <see cref="IntegerMathConverter" /> and
+///     <see cref="DoubleMathConverter" />.
+/// </summary>
+public enum Calculation
+{
+    /// <summary>
+    ///     The property shall be added to the value.
+    /// </summary>
+    Addition,
+
+    /// <summary>
+    ///     The property shall be subtracted from the value.
+    /// </summary>
+    Subtraction,
+
+    /// <summary>
+    ///     The property shall be multiplied with the value.
+    /// </summary>
+    Multiplication,
+
+    /// <summary>
+    ///     The property shall be divided from the value.
+    /// </summary>
+    Division
+}
